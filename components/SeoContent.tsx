56e603598ee2d6d4e7414434b81c211968a8b845
yokoyamahirokazu <h_yokoyama@side-creative.com>
2022-03-01 12:18:23 +0900

    meta変更

--- components/SeoContent.tsx
+++ components/SeoContent.tsx
@@ -10,14 +10,13 @@ interface Props {
 }
 
 const SeoContent = ({ pageTitle, pageDescription, pageUrl, ogpImg, noIndex }: Props) => {
-  const defaultTitle =
-    '遠隔接客サービス RURA｜まるで瞬間移動。 高スキルのスタッフを全国に０秒派遣。 お店の無人化や人材不足に効果を発揮。';
+  const defaultTitle = 'CocoDeskオンライン相談サービス';
   const defaultDescription =
-    '時給450円から店舗スタッフを採用可能遠隔接客サービス RURA※特許出願中。RURAはロボットやバーチャルキャラクター越しに接客を行なうシステムで、全国各地にいるRURAワーカーが、WEB経由で店舗の接客。一人のスタッフが複数店舗をまたいで接客が可能';
+    '駅ナカや商業施設で展開する「CocoDesk」というワークブースを活用し、遠隔地にいる専門家（サービス担当者）にオンラインで相談できるサービスです。通勤や通学、お買い物の際に、いつでも手軽に利用でき、予約での利用も可能です。また、さまざまな事業者のサービスを1つのブースで利用できるのも特徴です。';
   const defaultPageUrl = config.baseUrl;
   const defaultOgpImg = `${config.baseUrl}/images/og_image.jpg`;
 
-  const title = pageTitle ? `${pageTitle} | 遠隔接客サービス RURA` : defaultTitle;
+  const title = pageTitle ? `${pageTitle} | CocoDeskオンライン相談サービス` : defaultTitle;
   const description = pageDescription ? pageDescription : defaultDescription;
   const url = pageUrl ? `${defaultPageUrl}${pageUrl}` : defaultPageUrl;
   const imgUrl = ogpImg ? ogpImg : defaultOgpImg;
@@ -34,7 +33,6 @@ const SeoContent = ({ pageTitle, pageDescription, pageUrl, ogpImg, noIndex }: Pr
       <meta property="og:type" content="website" />
       <meta property="og:image" content={imgUrl} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@RURAtimeleap" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link rel="canonical" href={url} />
       {noIndex && <meta name="robots" content="noindex" />}
